bccf1e1b40515ac22c6da5b3c2692ec3eab56865
Davazano <davideoguche@gmail.com>
2024-06-18 15:37:29 +0100

    Pygame setup

---
+++ code/main.py
@@ -0,0 +1,22 @@
+import pygame
+
+# general setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
+running = True
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw the game
+    display_surface.fill((0, 127, 127))
+    pygame.display.update()
+
+pygame.quit()
+quit()

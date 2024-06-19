2ef7764ef3299bffc8b4c29ee0902f813657e2a2
Davazano <davideoguche@gmail.com>
2024-06-19 21:32:54 +0100

    Bounce player left and right

--- code/main.py
+++ code/main.py
@@ -21,7 +21,16 @@ player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
 star_surf = pygame.image.load(join("space_shooter", "images", "star.png")).convert_alpha()
 star_position = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
+# meteor
+meteor_surf = pygame.image.load(join("space_shooter", "images", "meteor.png")).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+# laser
+laser_surf = pygame.image.load(join("space_shooter", "images", "laser.png")).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
+
 running = True
+player_direction = 1
 
 while running:
     # event loop
@@ -35,9 +44,14 @@ while running:
     for pos in star_position:
         display_surface.blit(star_surf, pos)
 
-    if player_rect.right < WINDOW_WIDTH:
-        player_rect.left += 0.2
-    
+    display_surface.blit(meteor_surf, meteor_rect)
+
+    display_surface.blit(laser_surf, laser_rect)
+
+    player_rect.x += 0.3 * player_direction
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+
     display_surface.blit(player_surf, player_rect)
 
     pygame.display.update()

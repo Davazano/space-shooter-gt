c724952a37e9b722ebb5edca1c39d71596c5aec4
Davazano <davideoguche@gmail.com>
2024-07-13 19:22:51 +0100

    Display text in the game

--- code/main.py
+++ code/main.py
@@ -102,6 +102,13 @@ def collisions():
             laser.kill()
             print(collided_sprites[0])
 
+def display_score():
+    current_time = pygame.time.get_ticks() // 100
+    text_surf = font.render(str(current_time), True, (240, 240, 240))
+    text_rect = text_surf.get_frect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50))
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, (240, 240, 240), text_rect.inflate(20, 10).move(0, -8), 5, 10)
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -114,6 +121,7 @@ clock = pygame.time.Clock()
 star_surf = pygame.image.load(join("space_shooter", "images", "star.png")).convert_alpha()
 meteor_surf = pygame.image.load(join("space_shooter", "images", "meteor.png")).convert_alpha()
 laser_surf = pygame.image.load(join("space_shooter", "images", "laser.png")).convert_alpha()
+font = pygame.font.Font(join("space_shooter", 'images', 'Oxanium-Bold.ttf'), 40)
 
 # sprites
 all_sprites = pygame.sprite.Group()
@@ -146,8 +154,12 @@ while running:
     collisions()
 
     # draw the game
-    display_surface.fill("darkgray")
+    display_surface.fill('#3a2e3f') 
+    display_score()
     all_sprites.draw(display_surface)
+
+    # draw test
+    # pygame.draw.rect(display_surface, 'red', player.rect, 10, 10)
 
     pygame.display.update()
 
